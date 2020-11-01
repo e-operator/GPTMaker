a2593d9d9ace6440ff87dce42aeff07f07e22414
e-operator <67810039+e-operator@users.noreply.github.com>
2020-11-01 04:50:06 -0500

    Improve progress bar

--- downloaderProgress.js
+++ downloaderProgress.js
@@ -1,8 +1,12 @@
 const {ipcRenderer} = require('electron');
 
 const downloadProgressContainer = document.getElementById('download-progress-container');
+const downloadProgressBar = document.getElementById('download-progress-bar');
 
 ipcRenderer.on('download-progress', (event, progress) => {
     console.log(progress);
-    downloadProgressContainer.innerHTML = "<p style='color: white;'>" + progress + "</p>";
+    if(progress >= 0 && progress != null){
+        downloadProgressContainer.innerHTML = "<p style='color: white;'>" + progress + "%</p>"
+        downloadProgressBar.style.width = progress + "%";
+    }
 });
